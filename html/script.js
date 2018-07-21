6ea720b5fdf0b4cd0d33dda59a78c8fd45452f22
Unknown <xpetrj02@stud.fit.vutbr.cz>
2018-07-21 20:29:28 +0200

    basic styling

--- html/script.js
+++ html/script.js
@@ -8,6 +8,24 @@ $("#hamburger").click(function(){
     $("#header").addClass("open");
 });
 
+$(function() {
+    var obsah = document.getElementById("header");
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 10) {
+            obsah.style = "background-color: #10263C; opacity:0.85; transition: 0.5s; padding-top: 10px; padding-bottom:10px; width:100%";
+            document.getElementById("head-logo").style = "margin-left: 10px; z-index:999;";
+            document.getElementById("hamburger").style = "margin-right: 10px; z-index:999;";
+            document.getElementById("tip-butt").style = "margin-right: 10px";
+        } else {
+          obsah.style = "background: transparent; transition: 0.5s; padding-top: 30px; padding-bottom:30px; width:85%";
+          document.getElementById("head-logo").style = "margin-left: 0px; z-index:999;";
+          document.getElementById("hamburger").style = "margin-right: 0px; z-index:999;";
+          document.getElementById("tip-butt").style = "margin-right: 0px";
+        }
+    });
+  });
+
 function openNav() {
     var element, name, arr;
     element = document.getElementById("header");
