1a537b0b13a4a94812862ef1644bd11c67595c47
Unknown <xpetrj02@stud.fit.vutbr.cz>
2018-07-24 16:29:09 +0200

    big revisition

--- html/script.js
+++ html/script.js
@@ -1,46 +1,31 @@
-/*function openNav(){
-  var x = document.getElementById("header");
-  //var head = document.getElementsByClassName("header");
-  x.className += "open";
-};*/
-
-$("#hamburger").click(function(){
-    $("#header").addClass("open");
-});
-
 $(function() {
-    var obsah = document.getElementById("header");
+    var x = document.getElementsByClassName("header");
+    obsah = x[0];
     $(window).scroll(function() {
         var scroll = $(window).scrollTop();
         if (scroll >= 10) {
-            obsah.style = "background-color: #10263C; opacity:0.85; transition: 0.5s; padding-top: 10px; padding-bottom:10px; width:100%";
-            document.getElementById("head-logo").style = "margin-left: 10px; z-index:999;";
-            document.getElementById("hamburger").style = "margin-right: 10px; z-index:999;";
-            document.getElementById("tip-butt").style = "margin-right: 10px";
+          obsah.style = "background-color: #10263C; opacity: 0.85; transition: 0.5s; padding-top: 10px; padding-bottom:10px;";
         } else {
-          obsah.style = "background: transparent; transition: 0.5s; padding-top: 30px; padding-bottom:30px; width:85%";
-          document.getElementById("head-logo").style = "margin-left: 0px; z-index:999;";
-          document.getElementById("hamburger").style = "margin-right: 0px; z-index:999;";
-          document.getElementById("tip-butt").style = "margin-right: 0px";
+          obsah.style = "background: transparent; transition: 0.5s; padding-top: 30px; padding-bottom:30px;";
         }
     });
   });
 
 function openNav() {
-    var element, name, arr;
-    element = document.getElementById("header");
-    name = "open";
-    arr = element.className.split(" ");
-    if (arr.indexOf(name) == -1) {
-        element.className += " " + name;
-    }
-
-  document.getElementById("hamburger").onclick = function() { closeNav(); }
+  var element, name, arr;
+  var x = document.getElementsByClassName("header");
+  element = x[0];
+  name = "open";
+  arr = element.className.split(" ");
+  if (arr.indexOf(name) == -1) {
+    element.className += " " + name;
+  }
+  document.getElementsByClassName("header-hamburger")[0].onclick = function() { closeNav(); }
 }
 
 function closeNav() {
-    var element = document.getElementById("header");
-    element.className = element.className.replace(/\bopen\b/g, "");
-
-  document.getElementById("hamburger").onclick = function() { openNav(); }
+  var x = document.getElementsByClassName("header");
+  var element = x[0];
+  element.className = element.className.replace(/\bopen\b/g, "");
+  document.getElementsByClassName("header-hamburger")[0].onclick = function() { openNav(); }
 }
